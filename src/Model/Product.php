9cddd5edcdf027ae980e23325c4eb2cbac073789
Elias Gasparis <iliasgas@gmail.com>
2025-09-21 15:37:52 +0300

    added validation to id and name

--- src/Model/Product.php
+++ src/Model/Product.php
@@ -6,12 +6,22 @@ use App\Model\Discount\DiscountStrategyInterface;
 
 readonly class Product implements \JsonSerializable
 {
+    private string $name;
+
     public function __construct(
         private ?int $id,
-        private string $name,
+        string $name,
         private Price $basePrice,
         private DiscountStrategyInterface $discountStrategy
     ) {
+        if ($id !== null && $id <= 0) {
+            throw new \InvalidArgumentException('ID must be a positive integer');
+        }
+
+        $this->name = trim($name);
+        if ($this->name === '' || mb_strlen($this->name) > 255) {
+            throw new \InvalidArgumentException('Name must be between 1 and 255 characters');
+        }
     }
 
     public function getId(): ?int
--- tests/Model/ProductTest.php
+++ tests/Model/ProductTest.php
@@ -6,10 +6,86 @@ use App\Model\Discount\FixedDiscountStrategy;
 use App\Model\Discount\NoDiscountStrategy;
 use App\Model\Price;
 use App\Model\Product;
+use InvalidArgumentException;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\TestWith;
 use PHPUnit\Framework\TestCase;
 
 class ProductTest extends TestCase
 {
+    #[TestWith([0])]
+    #[TestWith([-1])]
+    public function testInvalidId(int $id): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('ID must be a positive integer');
+
+        new Product(
+            $id,
+            'Apple',
+            new Price(100),
+            new NoDiscountStrategy()
+        );
+    }
+
+    #[DataProvider('invalidNameProvider')]
+    public function testInvalidName(string $name): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage('Name must be between 1 and 255 characters');
+
+        new Product(
+            123,
+            $name,
+            new Price(100),
+            new NoDiscountStrategy()
+        );
+    }
+
+    public static function invalidNameProvider(): array
+    {
+        return [
+            'empty string' => [''],
+            'whitespace' => [' '],
+            'too long' => [str_repeat('a', 256)],
+            'too long with multibyte characters' => [str_repeat('读', 256)],
+        ];
+    }
+
+    #[DataProvider('validNameProvider')]
+    public function testValidName(string $name): void
+    {
+        $product = new Product(
+            123,
+            $name,
+            new Price(100),
+            new NoDiscountStrategy()
+        );
+
+        self::assertSame($name, $product->getName());
+    }
+
+    public static function validNameProvider(): array
+    {
+        return [
+            'min length' => ['1'],
+            'max length' => [str_repeat('a', 255)],
+            'max length with multibyte characters' => [str_repeat('读', 255)],
+        ];
+    }
+
+    public function testNameIsTrimmed(): void
+    {
+        $product = new Product(
+            123,
+            '     Apple         ',
+            new Price(100),
+            new NoDiscountStrategy()
+        );
+
+        self::assertSame('Apple', $product->getName());
+    }
+
     public function testGetters(): void
     {
         $discountStrategy = new NoDiscountStrategy();
